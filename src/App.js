614e9f65738b1249c4255f46ad268f3e3abbda93
Drew Keat <thedrewkeat@gmail.com>
2021-11-25 21:21:08 -0700

    commit tree 2ff6a336ea09692e1f44c9959a4324d78ca9790e

--- src/App.js
+++ src/App.js
@@ -1,1 +1,41 @@
 // create your App component here
+import React, { Component } from 'react'
+
+export class App extends Component {
+  constructor() {
+    super()
+  
+    this.state = {
+       people: [],
+    }
+  }
+  
+  getPeople = () => {
+    fetch('http://api.open-notify.org/astros.json').then(resp => resp.json()).then(data => {
+      this.setState({
+        people: data.people,
+      })
+    })
+  }
+
+  generateList = () => {
+    return this.state.people.map((person, index) => <li key={`item-${index}`}>{person.name}</li>)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>These people are currently in space!</h1>
+        <ul>
+          {this.generateList()}
+        </ul>
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    this.getPeople()
+  }
+}
+
+export default App
